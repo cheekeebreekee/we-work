7403872ba246c6546ce2af9005b09b9ce9ffa5ec
Yahor Simak <simak_yahor@mail.ru>
2017-11-03 00:29:37 +0300

    we-work/feature: move scripts to index.js

--- scripts.js
+++ scripts.js
@@ -21,7 +21,12 @@ $(function() {
   });
   $('.project-select-profession-btn + .proffesions-wrapper .apply-btn').click(function (e) {
     e.preventDefault();
-    $('.project-select-profession-btn').removeClass('expanded');
+    var newProfession = $('.new-project-wrapper .profession-input').val();
+    $('.new-project-wrapper .profession-input').val('');
+    var proffessionBlock = '<div class="selected-professions-item"><span>' + newProfession + '</span><span class="selected-professions-close"></span></div>';
+    if (newProfession.length) {
+      $('.project-selected-professions').append(proffessionBlock);
+    }
   });
   $('.project-select-attachment-btn + .attachment-wrapper .apply-btn').click(function (e) {
     e.preventDefault();
@@ -30,16 +35,56 @@ $(function() {
   $('.new-category').click(function () {
     $(this).hide();
   });
-  // $( ".project-category" ).change(function( event ) {
-  //   var newProjectInput = $(this).parent().clone(true, true);
-  //   console.log(newProjectInput);
-  //   $($(this).parent().parent()).after(newProjectInput);
-  //   $(newProjectInput).find('.jq-selectbox').removeClass('opened').removeClass('chanfed').removeClass('focused');
-  //   $( this ).off( event );
-  // });
-  // $('.project-category option').one('click', function () {
-  //   var newProjectInput = $(this).parent().parent();
-  //   console.log(newProjectInput);
-  //   $(this).parent().parent().insertAfter($(this).parent().parent())
-  // })
+  function initDoubleSelect($parentSelect, removeOld){
+      var parentVal = $parentSelect.val(),
+          $parentContainer = $parentSelect.parents('.jq-selectbox');
+
+      if(removeOld){
+          $parentContainer.siblings('.select_child').remove();
+      }
+
+      if(!parentVal){
+          return;
+      }
+
+      var childVal = $parentSelect.data('child-val'),
+          dataVar = $parentSelect.data('var-name'),
+          childName = $parentSelect.data('child-name'),
+          disabled = $parentSelect.attr('disabled') == 'disabled',
+          childSelect = '';
+
+      var bordered = $parentContainer.hasClass('bordered') ? 'bordered' : '';
+      childSelect += '<select class="select_styler select_child ' + bordered + '" name="' + childName + '" ';
+      childSelect += disabled ? ' disabled="disabled"' : '';
+      childSelect += '>';
+      childSelect += '<option value="">(не выбрано)</option>';
+      if(typeof dataVar != 'undefined' && dataVar.length > 0 && typeof WW_DBL_SELECT == 'object'){
+          if(typeof WW_DBL_SELECT[dataVar] == 'object'){
+              if(typeof WW_DBL_SELECT[dataVar][parentVal] == 'object'){
+                  $.each(WW_DBL_SELECT[dataVar][parentVal], function(key, optionData){
+                      childSelect += '<option value="' + optionData.VALUE + '"';
+                      if(childVal == optionData.VALUE){
+                          childSelect += ' selected="selected"';
+                      }
+                      childSelect += '>' + optionData.LABEL + '</option>';
+                  });
+              }
+          }
+      }
+      childSelect += '</select>';
+
+      $parentContainer.after(childSelect);
+      $parentContainer.siblings('.select_child').styler();
+  }
+
+  $('.select_parent').each(function(){
+      initDoubleSelect($(this), false);
+  });
+
+  $('.select_parent').on('change', function(){
+      initDoubleSelect($(this), true);
+  });
+  $('.btn_hint').on('click', function(){
+		$(this).find($('.popover_text')).toggleClass('active');
+	});
 });
